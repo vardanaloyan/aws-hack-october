c9098baeee8cba8b994d922c8653bd48200b072b
Vardan <vardan.aloyan@swisscom.com>
2022-10-18 14:58:29 +0200

    added systemd service file

--- src/main.py
+++ src/main.py
@@ -9,5 +9,5 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="debug", debug=True,
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug", debug=True,
                 workers=1, limit_concurrency=1, limit_max_requests=1)
